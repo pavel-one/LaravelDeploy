ae26ec8e041afa9ab78e27607cbf6caf324bd7d5
Pavel Zarubin <pavel@orendat.ru>
2021-10-23 16:42:25 +0300

    Тесты

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo "🔑 Добавляю SSH ключ..." &&
-eval $(ssh-agent -s) &&
-ssh-add <(echo "${INPUT_KEY}") &&
-echo "🔐 Ключ добавлен";
+RSA_DIR="${HOME}/.ssh/id_rsa"
+#INPUT_KEY
+
+echo "🔑 Добавляю SSH ключ..."
+touch RSA_DIR
+echo -e "${INPUT_KEY}" >> RSA_DIR
+echo "🔐 Ключ добавлен"
 
 ssh ${INPUT_USER}@${INPUT_HOST} "bash ${INPUT_DIR}bin/user.sh"
