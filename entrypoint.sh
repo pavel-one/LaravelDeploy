17cbf01f23cb99d4cc6bdec8fe9878126099fd30
Pavel Zarubin <pavel@orendat.ru>
2021-10-23 16:44:18 +0300

    Тесты

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,9 @@ RSA_DIR="${HOME}/.ssh/id_rsa"
 #INPUT_KEY
 
 echo "🔑 Добавляю SSH ключ..."
-touch RSA_DIR
-echo -e "${INPUT_KEY}" >> RSA_DIR
+touch $RSA_DIR
+echo -e "${INPUT_KEY}" >> $RSA_DIR
+chmod 600 $RSA_DIR
 echo "🔐 Ключ добавлен"
 
 ssh ${INPUT_USER}@${INPUT_HOST} "bash ${INPUT_DIR}bin/user.sh"
