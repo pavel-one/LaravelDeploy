9eaa586f8e16635138623de3be8548e39a592c7c
Pavel Zarubin <pavel@orendat.ru>
2021-10-23 20:38:57 +0300

    Тесты

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 mkdir -p ~/.ssh/
 echo "$INPUT_KEY" > ~/.ssh/id_rsa
-sudo chmod 600 ~/.ssh/private.key
+chmod 600 ~/.ssh/private.key
 
 ssh -o "StrictHostKeyChecking=no" ${INPUT_USER}@${INPUT_HOST} "bash ${INPUT_DIR}bin/user.sh"
